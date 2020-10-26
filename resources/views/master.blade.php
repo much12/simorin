c420b89192439f811159ec992c9a0a808ef2d039
Nakula Zanuar <nakulazanuar666@gmail.com>
2020-10-26 11:45:33 +0700

    update 26-10

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -17,8 +17,9 @@
       <link href="{{ asset('assets/fontawesome/css/fontawesome.css') }}" rel="stylesheet">
       <link href="{{ asset('assets/fontawesome/css/brands.css') }}" rel="stylesheet">
       <link href="{{ asset('assets/fontawesome/css/solid.css') }}" rel="stylesheet">
+      <link href="{{ asset('assets/fontawesome/css/all.css') }}" rel="stylesheet">
    </head>
-   <body class="theme-red">
+<body class="theme-red" style=" background-image: url('{{asset('assets/images/bgwa.jpg') }}'); background-size: 50rem; ">
       <div class="page-loader-wrapper">
          <div class="loader">
             <div class="preloader">
@@ -38,123 +39,14 @@
       <nav class="navbar">
          <div class="container-fluid">
             <div class="navbar-header">
-               <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                <a href="javascript:void(0);" class="bars"></a>
                <a class="navbar-brand" href="index.html">SIMORIN</a>
             </div>
          </div>
       </nav>
-      <section>
-         <aside id="leftsidebar" class="sidebar">
-            <div class="user-info">
-               <div class="image">
-                  <img src="{{asset('assets/images/user.png')}}" width="48" height="48" alt="User" />
-               </div>
-               <div class="info-container">
-                  <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Name User</div>
-                  <div class="email">email.@gmail.com</div>
-                  <div class="btn-group user-helper-dropdown">
-                     <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
-                     <ul class="dropdown-menu pull-right">
-                        <li><a href="javascript:void(0);"><i class="material-icons">person</i>Profile</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="javascript:void(0);"><i class="material-icons">input</i>Sign Out</a></li>
-                     </ul>
-                  </div>
-               </div>
-            </div>
-            <div class="menu">
-               <ul class="list">
-                  <li class="active">
-                     <a>
-                        <i class="fa fa-home"></i>
-                     <span>Home</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">text_fields</i>
-                     <span>Typography</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">layers</i>
-                     <span>Helper Classes</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">widgets</i>
-                     <span>Widgets</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">swap_calls</i>
-                     <span>User Interface (UI)</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">assignment</i>
-                     <span>Forms</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">view_list</i>
-                     <span>Tables</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">perm_media</i>
-                     <span>Medias</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">pie_chart</i>
-                     <span>Charts</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">content_copy</i>
-                     <span>Example Pages</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">map</i>
-                     <span>Maps</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">trending_down</i>
-                     <span>Multi Level Menu</span>
-                     </a>
-                  </li>
-                  <li>
-                     <a>
-                     <i class="material-icons">update</i>
-                     <span>Changelogs</span>
-                     </a>
-                  </li>
-               </ul>
-            </div>
-            <div class="legal">
-               <div class="copyright">
-                &copy; 2020 SKARIGA. All Right Reserved</a>.
-               </div>
-               <div class="version">
-                  <b>Version: </b> 1.0.0-beta
-               </div>
-            </div>
-         </aside>
-      </section>
+
+      @include('sidebar')
+
       <section class="content">
          <div class="container-fluid">
             <div class="block-header">
