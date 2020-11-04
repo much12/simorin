c6eefdd06e53f51e264068ff78a5ef6677dc1a63
Muhammad Andika <dikapolk@gmail.com>
2020-11-04 13:38:46 +0700

    Add Proses Login

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,8 @@ Route::get('/', function () {
     return view('dashboard.index');
 });
 
-Route::get('login', function () {
-    return view('login');
-});
+Route::get('login','LoginController@index');
+Route::get('login/proses','LoginController@proses');
 
 Route::get('company', 'CompanyController@index');
 Route::get('company/add', 'CompanyController@modal_add');
