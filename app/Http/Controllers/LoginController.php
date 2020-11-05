359976482424e8443c1adeeda36fd0b7570dffc2
Muhammad Andika <dikapolk@gmail.com>
2020-11-05 09:40:52 +0700

    Adding Session Login and Logout Function

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use KKSI;
 use App\PembimbingPerusahaan;
 use Session;
+use DB;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -23,20 +25,36 @@ class LoginController extends Controller
         $admin = $this->cek_admin($email, $password);
 
         if ($pembimbing != null) {
-            return JSONResponseDefault(KKSI::OK, 'Login Pembimbing Berhasil');
+            Session::put('is_login',true);
+            Session::put('id',$pembimbing->id);
+            Session::put('nama',$pembimbing->nama_pembimbing);
+            Session::put('role','Perusahaan');
+            return JSONResponseDefault(KKSI::OK, 'Login Berhasil');
         } else if ($admin != null) {
-            return JSONResponseDefault(KKSI::OK, 'Login Admin Berhasil');
+            Session::put('is_login',true);
+            Session::put('id',$admin->id);
+            Session::put('nama',$admin->nama_admin);
+            Session::put('role','Admin');
+            return JSONResponseDefault(KKSI::OK, 'Login Berhasil');
         } else {
-            return JSONResponseDefault(KKSI::FAILED, 'Access Denied');
+            return JSONResponseDefault(KKSI::FAILED, 'Email atau Password Salah');
         }
     }
 
     public function cek_admin($email, $password)
     {
+        return DB::table('msadmin')->where('email',$email)->where('password',$password)->first();
     }
 
     public function cek_pembimbing_perusahaan($email, $password)
     {
         return PembimbingPerusahaan::where('email', $email)->where('password', $password)->first();
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+        return redirect('login');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,8 @@
    <link rel="icon" href="{{asset('assets/favicon.ico')}}" type="image/x-icon">
 
    <!-- Google Fonts -->
-   <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
+   <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet"
+      type="text/css">
    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
    <!-- Bootstrap Core Css -->
@@ -59,7 +60,8 @@
 
       <div class="card">
          <div class="body">
-            <form id="frmLogin" action="{{url()->current()}}/proses" autocomplete="off" onsubmit="login();return false;">
+            <form id="frmLogin" action="{{url()->current()}}/proses" autocomplete="off"
+               onsubmit="login();return false;">
                <div class="msg"><b>LOGIN</b></div>
                <div class="input-group">
                   <span class="input-group-addon">
@@ -67,7 +69,8 @@
                   </span>
 
                   <div class="form-line">
-                     <input type="email" class="form-control" name="email" id="email" placeholder="Email" required autofocus>
+                     <input type="email" class="form-control" name="email" id="email" placeholder="Email" required
+                        autofocus>
                   </div>
                </div>
 
@@ -77,7 +80,8 @@
                   </span>
 
                   <div class="form-line">
-                     <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+                     <input type="password" class="form-control" name="password" id="password" placeholder="Password"
+                        required>
                   </div>
                </div>
 
@@ -123,6 +127,9 @@
                console.log(response);
                if (response.RESULT == 'OK') {
                   swalMessageSuccess(response.MESSAGE);
+                  setTimeout(function() {
+                     location.href = 'dashboard';
+                  }, 1000);
 
                } else {
                   swalMessageFailed(response.MESSAGE);
